9d3c8c1c84de9887e0273e020a199fb2b447b0c8
PuddingCoke <zhangxianzhe1231@outlook.com>
2022-03-02 14:09:57 +0800

    添加项目文件。

---
+++ Sudoku/main.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+#include<vector>
+#include<chrono>
+
+using namespace std;
+
+int matrix[9][9] =
+{
+	{8,0,0,0,0,0,0,0,0},
+	{0,0,3,6,0,0,0,0,0},
+	{0,7,0,0,9,0,2,0,0},
+	{0,5,0,0,0,7,0,0,0},
+	{0,0,0,0,4,5,7,0,0},
+	{0,0,0,1,0,0,0,3,0},
+	{0,0,1,0,0,0,0,6,8},
+	{0,0,8,5,0,0,0,1,0},
+	{0,9,0,0,0,0,4,0,0}
+};
+
+class Point
+{
+public:
+	Point(const int& x, const int& y) :x(x), y(y) {}
+	const int x;
+	const int y;
+};
+
+void printMatrix()
+{
+	for (size_t i = 0; i < 9; i++)
+	{
+		for (size_t j = 0; j < 9; j++)
+		{
+			cout << matrix[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	cout << "\n";
+}
+
+vector<Point> points;
+
+void DFS(size_t index)//[x][y]
+{
+	const int& x = points[index].x;
+	const int& y = points[index].y;
+
+	for (int i = matrix[x][y] + 1; i < 10; i++)//遍历行列以及九宫格
+	{
+		bool flags = false;
+		for (int j = 0; j < 9; j++)
+		{
+			if (matrix[j][y] == i || matrix[x][j] == i || matrix[x / 3 * 3 + j / 3][y / 3 * 3 + j % 3] == i)
+			{
+				flags = true;
+				break;
+			}
+		}
+
+		//有可放置的数字
+
+		if (!flags) {
+			matrix[x][y] = i;
+			if (++index == points.size())//是否完成
+			{
+				return;
+			}
+			DFS(index);//遍历下一个空格
+			return;
+		}
+
+	}
+
+	//没有可放置的数字
+	matrix[x][y] = 0;
+	DFS(--index);
+
+	return;
+}
+
+int main()
+{
+	chrono::high_resolution_clock timer;
+
+	std::chrono::steady_clock::time_point timeStart;
+
+	std::chrono::steady_clock::time_point timeEnd;
+
+	printMatrix();
+
+	timeStart = timer.now();
+
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			if (matrix[i][j] == 0)
+			{
+				points.push_back(Point(i, j));
+			}
+		}
+	}
+
+	DFS(0);
+
+	timeEnd = timer.now();
+
+	printMatrix();
+
+	cout << "Time " << chrono::duration_cast<std::chrono::milliseconds>(timeEnd - timeStart).count() << "ms\n";
+
+	cin.get();
+	return 0;
+}
